d00e3cfeac1347edfc0f91b6bb0348ca235f7ac8
RSAdrian9 <adrian4rs@gmail.com>
2024-03-07 23:04:56 +0100

    Changes in Transaction

--- src/main/java/org/Aruiz/model/Transaction.java
+++ src/main/java/org/Aruiz/model/Transaction.java
@@ -44,12 +44,105 @@ public class Transaction {
     @JoinColumn(name = "book_id")
     private Book book;
 
+    public Transaction() {
+
+    }
+
     // Constructor, getters y setters
 
     // Método para calcular el precio total
     @Transient // No persistir en la base de datos
     public void calculateTotalPrice() {
         this.totalPrice = this.quantity * this.unitPrice;
+    }
+
+    public Transaction(int id, LocalDateTime date, int quantity, double unitPrice, double totalPrice, TransactionStatus status, User user, Book book) {
+        this.id = id;
+        this.date = date;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.user = user;
+        this.book = book;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", date=" + date +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                ", totalPrice=" + totalPrice +
+                ", status=" + status +
+                ", user=" + user +
+                ", book=" + book +
+                '}';
     }
 
     @Entity
@@ -60,13 +153,30 @@ public class Transaction {
 
         @Column(name = "shipping_address")
         private String shippingAddress; // Dirección de envío para compras
+
+        public Purchase(int id, LocalDateTime date, int quantity, double unitPrice, double totalPrice, TransactionStatus status, User user, Book book) {
+            super(id, date, quantity, unitPrice, totalPrice, status, user, book);
+        }
+
+        public Purchase() {
+            super();
+        }
     }
 
     @Entity
     @DiscriminatorValue("venta")
     class Sale extends Transaction {
+        public Sale(int id, LocalDateTime date, int quantity, double unitPrice, double totalPrice, TransactionStatus status, User user, Book book) {
+            super(id, date, quantity, unitPrice, totalPrice, status, user, book);
+        }
+
+        public Sale() {
+            super();
+        }
         // Atributos específicos de las ventas
     }
+
+
 }
 
 
